e345484fa2d27aabe80d9e6d7e0787f23ba13088
KNtanos <86077849+Kntanos@users.noreply.github.com>
2022-01-27 15:26:25 +0000

    Add rest of CRUD actions for completion

--- APITechTest/Contollers/PlayersController.cs
+++ APITechTest/Contollers/PlayersController.cs
@@ -19,15 +19,47 @@ namespace APITechTest.Contollers
         }
 
         [HttpGet]
-        public async Task<IEnumerable<Player>> Get()
+        public async Task<IEnumerable<Player>> GetPlayers()
         {
             return await _PlayerRepository.Get();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<Player>> Get(int id)
+        public async Task<ActionResult<Player>> GetPlayers(int id)
         {
             return await _PlayerRepository.Get(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Player>> PostPlayer(Player player)
+        {
+            var newPlayer = await _PlayerRepository.Create(player);
+            return CreatedAtAction(nameof(GetPlayers), new { id = newPlayer.Id }, newPlayer);
+        }
+
+        // Added rest of CRUD actions for completion
+        [HttpPut]
+        public async Task<ActionResult> PutBooks(int id, [FromBody] Player player)
+        {
+            if (id != player.Id)
+            {
+                return BadRequest();
+            }
+
+            await _PlayerRepository.Update(player);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var playerToDelete = await _PlayerRepository.Get(id);
+            if (playerToDelete == null)
+                return NotFound();
+
+            await _PlayerRepository.Delete(playerToDelete.Id);
+            return NoContent();
         }
     }
 }
